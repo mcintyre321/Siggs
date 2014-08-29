7815aa2c44112ec20492bc0e808ad1e81483920e
mcintyre321 <mcintyre321@gmail.com>
2014-08-29 17:27:26 +0100

    Updated siggs

--- Siggs/SiggsExtensions.cs
+++ Siggs/SiggsExtensions.cs
@@ -14,104 +14,110 @@ namespace Siggs
     {
         private static AssemblyBuilder asmBuilder;
         private static ModuleBuilder modBuilder;
-        static ConcurrentDictionary<string, Type> typeCache = new ConcurrentDictionary<string, Type>();
+        private static ConcurrentDictionary<string, Type> typeCache = new ConcurrentDictionary<string, Type>();
 
         static SiggsExtensions()
         {
             asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("MethodSignatures"), AssemblyBuilderAccess.RunAndSave);
 
             modBuilder = asmBuilder.DefineDynamicModule("MethodSignatures", "MethodSignatures.dll");
-          
+
 
         }
+
         public static Type GetTypeForMethodInfo(this MethodInfo mi)
         {
-            var name = mi.DeclaringType.FullName + "." + mi.Name;
-            return typeCache.GetOrAdd(name, (s) =>
-            {
-                // Our intermediate language generator
-                ILGenerator ilgen;
+            var methodName = mi.Name;
+            var classFullName = mi.DeclaringType.FullName;
+            var parameterInfos =
+                mi.GetParameters().Select(pi => new GenericParameterInfo { Name = pi.Name, ParameterType = pi.ParameterType, GetCustomAttributes = () => pi.GetCustomAttributesData().ToArray() });
+            var key = classFullName + "." + methodName;
+            return GetTypeForGenericMethodInfo(key, methodName, classFullName, parameterInfos);
+        }
+ 
 
-                TypeBuilder typeBuilder = modBuilder.DefineType(name, TypeAttributes.Class | TypeAttributes.Public);
-
-                // The default constructor
-                typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
-                foreach (var parameterInfo in mi.GetParameters())
-                {
-                    var propertyName = parameterInfo.Name;
-                    if (mi.Name.StartsWith("set_")) propertyName = mi.Name.Substring(4);
-                    FieldBuilder backingField = typeBuilder.DefineField("_" + propertyName, parameterInfo.ParameterType, FieldAttributes.Private);
-                    PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, System.Reflection.PropertyAttributes.
-                                                                              HasDefault, parameterInfo.ParameterType, null);
-                    
-                    foreach (var customAttributeData in parameterInfo.GetCustomAttributesData())
-                    {
-                        var ctorArgs = customAttributeData.ConstructorArguments.Select(c => c.Value);
-                        var namedArguments = customAttributeData.NamedArguments;
-                        CustomAttributeData data = customAttributeData;
-                        var namedProperties = namedArguments.Where(a => !a.IsField).Select(a =>
-                            new
-                            {
-                                Property = data.AttributeType.GetProperty(a.MemberName),
-                                Value = a.TypedValue.Value
-                            });
-                        var namedFields = namedArguments.Where(a => a.IsField).Select(a =>
-                                                    new
-                                                    {
-                                                        Field = data.AttributeType.GetField(a.MemberName),
-                                                        Value = a.TypedValue.Value
-                                                    });
-
-                        var cab = new CustomAttributeBuilder(
-                            customAttributeData.Constructor, 
-                            ctorArgs.ToArray(),
-                            namedProperties.Select(p => p.Property).ToArray(),
-                            namedProperties.Select(p => p.Value).ToArray(),
-                            namedFields.Select(p => p.Field).ToArray(),
-                            namedFields.Select(p => p.Value).ToArray()
-                            );
-                        propertyBuilder.SetCustomAttribute(cab);
-                    }
-
-
-                    // Custom attributes for get, set accessors
-                    MethodAttributes getSetAttr = MethodAttributes.Public | MethodAttributes.HideBySig |
-                                                  MethodAttributes.SpecialName;
-
-                    MethodBuilder getter = typeBuilder.DefineMethod("get_" + propertyName, getSetAttr, parameterInfo.ParameterType, Type.EmptyTypes);
-
-                    // Code generation
-                    ilgen = getter.GetILGenerator();
-                    ilgen.Emit(OpCodes.Ldarg_0);
-                    ilgen.Emit(OpCodes.Ldfld, backingField); 
-                    ilgen.Emit(OpCodes.Ret);
-
-                    MethodBuilder setter = typeBuilder.DefineMethod("set_" + propertyName, getSetAttr, null, new Type[] {parameterInfo.ParameterType});
-
-                    // Code generation
-                    ilgen = setter.GetILGenerator();
-                    ilgen.Emit(OpCodes.Ldarg_0);
-                    ilgen.Emit(OpCodes.Ldarg_1);
-                    ilgen.Emit(OpCodes.Stfld, backingField); // setting the firstname field from the first argument (1)
-                    ilgen.Emit(OpCodes.Ret);
-
-
-                    propertyBuilder.SetGetMethod(getter);
-                    propertyBuilder.SetSetMethod(setter);
-
-
-                    
-                }
-                typeBuilder.CreateType();
-                var type = asmBuilder.GetType(name);
-                return type;
-            });
+        public static Type GetTypeForGenericMethodInfo(string fullnameAndMethodName, string methodName, string classFullName, IEnumerable<GenericParameterInfo> parameterInfos)
+        {
+            return typeCache.GetOrAdd(fullnameAndMethodName, (k) => BuildType(methodName, classFullName, parameterInfos));
         }
 
-        // Define other methods and classes here
+        private static Type BuildType(string methodName, string classFullName, IEnumerable<GenericParameterInfo> parameterInfos)
+        {
+            var fullnameAndMethodName = classFullName + "." + methodName;
 
+            // Our intermediate language generator
+            ILGenerator ilgen;
+
+            TypeBuilder typeBuilder = modBuilder.DefineType(fullnameAndMethodName, TypeAttributes.Class | TypeAttributes.Public);
+
+            // The default constructor
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+
+
+            foreach (var parameterInfo in parameterInfos)
+            {
+                var propertyName = parameterInfo.Name;
+                if (methodName.StartsWith("set_")) propertyName = methodName.Substring(4);
+                FieldBuilder backingField = typeBuilder.DefineField("_" + propertyName, parameterInfo.ParameterType, FieldAttributes.Private);
+                PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, System.Reflection.PropertyAttributes.HasDefault, parameterInfo.ParameterType, null);
+
+                foreach (var customAttributeData in parameterInfo.GetCustomAttributes())
+                {
+                    var ctorArgs = customAttributeData.ConstructorArguments.Select(c => c.Value);
+                    var namedArguments = customAttributeData.NamedArguments;
+                    CustomAttributeData data = customAttributeData;
+                    var namedProperties = namedArguments.Where(a => !a.IsField).Select(a => new {Property = data.AttributeType.GetProperty(a.MemberName), Value = a.TypedValue.Value});
+                    var namedFields = namedArguments.Where(a => a.IsField).Select(a => new {Field = data.AttributeType.GetField(a.MemberName), Value = a.TypedValue.Value});
+
+                    var cab = new CustomAttributeBuilder(customAttributeData.Constructor, ctorArgs.ToArray(), namedProperties.Select(p => p.Property).ToArray(),
+                        namedProperties.Select(p => p.Value).ToArray(), namedFields.Select(p => p.Field).ToArray(), namedFields.Select(p => p.Value).ToArray());
+                    propertyBuilder.SetCustomAttribute(cab);
+                }
+
+
+                // Custom attributes for get, set accessors
+                MethodAttributes getSetAttr = MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName;
+
+                MethodBuilder getter = typeBuilder.DefineMethod("get_" + propertyName, getSetAttr, parameterInfo.ParameterType, Type.EmptyTypes);
+
+                // Code generation
+                ilgen = getter.GetILGenerator();
+                ilgen.Emit(OpCodes.Ldarg_0);
+                ilgen.Emit(OpCodes.Ldfld, backingField);
+                ilgen.Emit(OpCodes.Ret);
+
+                MethodBuilder setter = typeBuilder.DefineMethod("set_" + propertyName, getSetAttr, null, new Type[] {parameterInfo.ParameterType});
+
+                // Code generation
+                ilgen = setter.GetILGenerator();
+                ilgen.Emit(OpCodes.Ldarg_0);
+                ilgen.Emit(OpCodes.Ldarg_1);
+                ilgen.Emit(OpCodes.Stfld, backingField); // setting the firstname field from the first argument (1)
+                ilgen.Emit(OpCodes.Ret);
+
+
+                propertyBuilder.SetGetMethod(getter);
+                propertyBuilder.SetSetMethod(setter);
+
+
+
+            }
+            typeBuilder.CreateType();
+            var type = asmBuilder.GetType(fullnameAndMethodName);
+            return type;
+        }
 
     }
+
+ 
+    public class GenericParameterInfo
+    {
+        public string Name { get; set; }
+        public Type ParameterType { get; set; }
+        public Func<CustomAttributeData[]> GetCustomAttributes { get; set; }
+
+    }
+
     internal class TypeDescriptorOverridingProvider : TypeDescriptionProvider
     {
         private readonly ICustomTypeDescriptor ctd;
@@ -126,13 +132,14 @@ namespace Siggs
             return ctd;
         }
     }
+
     internal class PropertyOverridingTypeDescriptor : CustomTypeDescriptor
     {
         private readonly Dictionary<string, PropertyDescriptor> overridePds = new Dictionary<string, PropertyDescriptor>();
 
-        public PropertyOverridingTypeDescriptor(ICustomTypeDescriptor parent)
-            : base(parent)
-        { }
+        public PropertyOverridingTypeDescriptor(ICustomTypeDescriptor parent) : base(parent)
+        {
+        }
 
         public void OverrideProperty(PropertyDescriptor pd)
         {
@@ -176,6 +183,7 @@ namespace Siggs
         {
             return GetPropertiesImpl(base.GetProperties());
         }
+
         public override PropertyDescriptorCollection GetProperties(Attribute[] attributes)
         {
             return GetPropertiesImpl(base.GetProperties(attributes));
